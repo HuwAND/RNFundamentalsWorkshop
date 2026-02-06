e77c8d45c312ebccf3fa2261dab2fbdadb35ec9e
Huw Tingley <huw.tingley@and.digital>
2026-02-06 13:14:57 +0000

    add code from exercise 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,15 @@
 import { Image, ScrollView, Text, View } from "react-native";
 
-const MovieCard = () => {
+const MovieCard = ({ image, title }: { image: string; title: string }) => {
   return (
     <View>
       <Image
         source={{
-          uri: "https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+          uri: image,
         }}
         style={{ width: 200, height: 300, marginRight: 8 }}
       />
-      <Text>Movie Card</Text>
+      <Text>{title}</Text>
     </View>
   );
 };
@@ -19,10 +19,22 @@ export default function MoviesScreen() {
     <View>
       <Text>MOST POPULAR MOVIES</Text>
       <ScrollView horizontal>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"
+          title="Sing 2"
+        />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg"
+          title="Spider-man. No way home."
+        />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BMzQ5ZDZhZDItZTNmZi00MWQ0LWJlNDUtZTE4ZWJmODNlM2Y3XkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_.jpg"
+          title="West Side Story"
+        />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BYzdlMTMyZWQtZWNmMC00MTJiLWIyMWMtM2ZlZDdlYzZhNTc0XkEyXkFqcGdeQXVyMTMzNDE5NDM2._V1_FMjpg_UX1000_.jpg"
+          title="House of Gucci"
+        />
       </ScrollView>
     </View>
   );
