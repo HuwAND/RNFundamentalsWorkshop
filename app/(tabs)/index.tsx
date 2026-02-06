76e6ebb94a5589040af1821e810807634d131442
Huw Tingley <huw.tingley@and.digital>
2026-02-06 13:10:04 +0000

    add code from exercise 1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,29 @@
-import { View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+
+const MovieCard = () => {
+  return (
+    <View>
+      <Image
+        source={{
+          uri: "https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+        }}
+        style={{ width: 200, height: 300, marginRight: 8 }}
+      />
+      <Text>Movie Card</Text>
+    </View>
+  );
+};
 
 export default function MoviesScreen() {
-  return (<View></View>);
+  return (
+    <View>
+      <Text>Movies Tab</Text>
+      <ScrollView horizontal>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </ScrollView>
+    </View>
+  );
 }
