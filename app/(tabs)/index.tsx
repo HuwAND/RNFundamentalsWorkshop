19425e778e278ff1771a66170e85fc35dde34f00
Huw Tingley <huw.tingley@and.digital>
2026-02-06 15:45:41 +0000

    add exercise 4 content

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { getPopularMovies, PopularMovie } from "@/api/movies";
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View, FlatList } from "react-native";
 
 const MovieCard = ({ image, title }: { image: string; title: string }) => {
   return (
@@ -31,24 +31,7 @@ export default function MoviesScreen() {
   return (
     <View>
       <Text>MOST POPULAR MOVIES</Text>
-      <ScrollView horizontal>
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"
-          title="Sing 2"
-        />
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg"
-          title="Spider-man. No way home."
-        />
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BMzQ5ZDZhZDItZTNmZi00MWQ0LWJlNDUtZTE4ZWJmODNlM2Y3XkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_.jpg"
-          title="West Side Story"
-        />
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BYzdlMTMyZWQtZWNmMC00MTJiLWIyMWMtM2ZlZDdlYzZhNTc0XkEyXkFqcGdeQXVyMTMzNDE5NDM2._V1_FMjpg_UX1000_.jpg"
-          title="House of Gucci"
-        />
-      </ScrollView>
+      <FlatList horizontal data={popularMovies} renderItem={({item}) => <MovieCard title={item.title} image={item.bannerImage} />}/>
     </View>
   );
 }
