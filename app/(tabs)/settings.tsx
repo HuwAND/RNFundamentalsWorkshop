7ea35fdc4a3838388a6027c82360bd41629c4434
Huw Tingley <huw.tingley@and.digital>
2026-02-19 14:49:37 +0000

    add code from exercise 10

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { useAuthorisation } from "@/context/AuthorisationContext";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function SettingsScreen() {
+  const { setIsAuthorised } = useAuthorisation();
+
+  const onLogoutPress = () => {
+    setIsAuthorised(false);
+  };
+
   return (
     <View>
       <Text>Settings Screen</Text>
+      <Pressable style={styles.button} onPress={onLogoutPress}>
+        <Text style={styles.buttonText}>LOGOUT</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: { backgroundColor: "red", padding: 10, margin: 20, borderRadius: 5 },
+  buttonText: { color: "white", textAlign: "center", fontWeight: "bold" },
+});
